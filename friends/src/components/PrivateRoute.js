31d8bb2ee3fd7dc4882a6e64dcb11a42e810d225
eric capiz <ericcapiz@gmail.com>
2020-10-03 16:22:37 -0500

    initial commit

---
+++ friends/src/components/PrivateRoute.js
@@ -0,0 +1,24 @@
+import React from "react";
+import { Route, Redirect } from "react-router-dom";
+
+
+const isUserAuthenticated = () => {
+  return localStorage.getItem("token") !== null;
+};
+
+const PrivateRoute = ({ component: Component, ...props }) => {
+  return (
+    <Route
+      {...props}
+      render={() => {
+        /* */
+        if (isUserAuthenticated()) {
+          return <Component />;
+        }
+        return <Redirect to="/login" />;
+      }}
+    />
+  );
+};
+
+export default PrivateRoute;
